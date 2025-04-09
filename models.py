816e6087e1081d8684e1dcd12f981f468f11580a
Sujata <sujataroy2009@gmail.com>
2025-04-09 21:51:36 +0530

    Update models.py via SDLC App on 2025-04-09T16:21:36.230950

---
+++ models.py
@@ -0,0 +1,9 @@
+class Snake:
+    def __init__(self, head, tail):
+        self.head = head
+        self.tail = tail
+
+class Ladder:
+    def __init__(self, bottom, top):
+        self.bottom = bottom
+        self.top = top
